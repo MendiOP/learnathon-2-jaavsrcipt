5448c8318283d09fd35073e7762544f52308690d
Mehedi Hasan <mehedibuet73@gmail.com>
2023-09-15 15:43:59 +0600

    First Class of JS

---
+++ Class 1/index.js
@@ -0,0 +1,27 @@
+// const person = {
+//     name: "munna",
+//     address: "Colleger Mor"
+// };
+
+// const clone = {
+//     ...person,
+//     occupation: "Student"
+// };
+
+// console.log(clone);
+
+// const arr = [1, 2, 3];
+// const arr2 = [5, 7, 3];
+
+// const result = [...arr, 232, ...arr2];
+// console.log(result);
+
+
+// const { location } = clone;
+
+// console.log(location);
+console.log(34);
+
+const fun = () => {
+    console.log(this);
+}
